75b88d74ce7a8f1ecfdc6fa3f2bdb53d3e1076bf
Brian <wbadyx@126.com>
2024-09-08 04:35:03 +0000

    update wt.stat.pl.
    Signed-off-by: Brian <wbadyx@126.com>

--- wt.stat.pl
+++ wt.stat.pl
@@ -1,5 +1,17 @@
 #!/usr/bin/perl
 
+
+#这段 Perl 代码是用来处理和分析一个制表符分隔的文本文件。让我简要解释一下它的功能：
+
+#它读取一个文件（通过命令行参数传入）。
+#对文件的每一行，它提取第一列（$type）和第五列（$descript）。
+#它统计每种 type 的总数，以及每种 type 下不同 descript 的出现次数。
+#最后，它输出统计结果。
+#输出格式为：|type|type总数|descript(descript出现次数)|
+
+
+
+
 my %count;
 my %hash;
 open IN,$ARGV[0] or die;
